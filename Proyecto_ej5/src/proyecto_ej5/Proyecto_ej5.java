f8dac246122860666db080e01a5c7fc619df52ed
andresmartinez6 <andresiz11x@gmail.com>
2023-05-12 13:19:50 +0200

    Hola esta es mi primera linea de codigo

---
+++ Proyecto_ej5/src/proyecto_ej5/Proyecto_ej5.java
@@ -0,0 +1,16 @@
+
+package proyecto_ej5;
+
+import java.util.Scanner;
+
+
+public class Proyecto_ej5 {
+
+  
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        
+        System.out.println("Primera linea de codigo");
+    }
+    
+}
